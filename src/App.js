9527124aa7712cf7ac2567db6b1e76f2a1280b77
Shubham Dhatwalia <shubhamdhatwalia3@gmail.com>
2024-11-10 13:21:12 +0530

    practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { Pagination, Stack } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import "./app.css";
 
 function App() {
+  const [tabledata, setTabledata] = useState();
+  const [currentPage, setcurrentPage] = useState(1);
+  const [rowsPerPage, setrowsPerPage] = useState(10);
+
+  const currentItems = tabledata?.products.slice(
+    (currentPage - 1) * rowsPerPage,
+    currentPage * rowsPerPage
+  );
+  const totalPages = Math.ceil(tabledata?.total / rowsPerPage);
+
+  useEffect(() => {
+    axios.get("https://dummyjson.com/products?limit=0").then((res) => {
+      setTabledata(res.data);
+    });
+  }, []);
+
+  const handlePageChange = (event, value) => {
+    setcurrentPage(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Welcome</h1>
+
+      <div className="bg-blue-100 border-blue-600">
+        <table className="table bg-slate-50 ml-auto mr-auto border border-black-100 my-3 ">
+          <thead>
+            <tr>
+              <th>Brand</th>
+              <th>Category</th>
+              <th>Image</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentItems?.map((values, index) => (
+              <tr key={index}>
+                <td>{values.title}</td>
+                <td>{values.category}</td>
+                <td>
+                  <LazyLoadImage
+                    src={values.images[0]}
+                    alt={values.title}
+                    width="50"
+                    effect="blur" // Adds a blur effect while loading
+                    placeholderSrc="https://via.placeholder.com/50" // Placeholder image
+                  />
+                </td>
+                <td>{values.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <Stack spacing={2} alignItems={"center"}>
+        <Pagination
+          count={totalPages}
+          page={currentPage}
+          onChange={handlePageChange}
+          color="primary"
+        />
+      </Stack>
+    </>
   );
 }
 
